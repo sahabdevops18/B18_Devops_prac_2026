ff24f57b21d7f24e2a2b430e852dbddcd4b6ff0d
sahabdevops18 <sahabdevops18@gmail.com>
2026-08-23 08:32:20 +0530

    Feature rg (#1)
    * my storage code
    
    * new update

--- main.tf
+++ main.tf
@@ -1,29 +1,23 @@
-terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "5.0.1"
-        }
-    }
-
-
+# 1. Resource Group
+resource "azurerm_resource_group" "rg1"{
+  name     = "Sahabrg1"
+  location = "central india"
 }
-
-provider "azurerm" {
-    features{}
+# 2. Storage Account
+resource "azurerm_storage_account" "storage" {
+  name                     = "sahabstorage2026" # Must be globally unique, lowercase letters and numbers only
+  resource_group_name      = azurerm_resource_group.rg1name
+  location                 = azurerm_resource_group.rg1location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    Environment = "Practice"
+    CreatedBy   = "Sahab"
+  }
 }
-
-resource "azurerm_resource_group" "rg1" {
-    name = "Testrg1"
-    location = "centralindia"
-}
-
-resource "azurerm_resource_group" "rg10" {
-    name = "Testrg10"
-    location = "centralindia"
-}
-
-resource "azurerm_resource_group" "rg100" {
-    name = "Testrg100"
-    location = "centralindia"
+# 3. Storage Blob Container
+resource "azurerm_storage_container" "containe0" {
+  name                  = "sahab-container" # Lowercase and hyphens only (no spaces)
+  storage_account_id    = azurerm_storage_account.storage.id
+  container_access_type = "private"
 }
